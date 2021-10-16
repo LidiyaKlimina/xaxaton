5f1448575d33e2d392a592449bd21b5c75cca3d0
dmitriigribanov <gds1984@bk.ru>
2021-10-16 19:09:34 +0500

    selenium_nalog for 1 inn finished

--- selenium_nalog_parse.py
+++ selenium_nalog_parse.py
@@ -1,15 +1,21 @@
+import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Safari()
 driver.get("https://egrul.nalog.ru/index.html")
 
-query_form = driver.find_element_by_id('query')
+query_form = driver.find_element(By.ID, 'query')
 query_form.clear()
-query_form.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-find_button = driver.find_element_by_id('btnSearch')
-driver.find_element_by_id("submit").click()
+query_form.send_keys("9705093554")
+
+find_button = driver.find_element(By.ID, 'btnSearch')
+find_button.click()
+
+time.sleep(3)
+data = driver.find_element(By.CLASS_NAME, 'res-text').text.split(',')
+data[0] = 'Адрес: ' + data[0]
+
 
 print(1)
